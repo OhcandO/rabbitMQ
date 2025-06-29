a4263fae2368ee73f83aea0d3aec0aa1797c7edc
JHOH <ohcando88@gmail.com>
2025-06-29 18:33:05 +0900

    minor change2

--- src/main/java/com/koji/rabbitmq1/config/MessageProducer.java
+++ src/main/java/com/koji/rabbitmq1/config/MessageProducer.java
@@ -9,14 +9,6 @@ import java.util.Queue;
 import java.util.function.Supplier;
 
 /**
- * packageName    : com.koji.rabbitmq1.config
- * fileName       : MessageProducer
- * date           : 2025-06-16
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2025-06-16                           최초 생성
  */
 @Component
 public class MessageProducer {
